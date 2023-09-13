286eca31fd735c87afde24266651f85f61fefb68
Youssef <yousefyousefyousef335@gmail.com>
2023-09-13 21:33:51 +0300

    theme and start notes viewing

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:note/Views/Notes_view.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: NotesApp(),
-    );
-  }
+  runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
@@ -21,6 +10,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NotesView(),
+    );
   }
 }
