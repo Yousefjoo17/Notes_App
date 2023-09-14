18320dc84ae477df356109f58316d802eba21603
Youssef <yousefyousefyousef335@gmail.com>
2023-09-15 00:58:02 +0300

    editnoteview

---
+++ lib/Views/Widgets/Edit_note_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditnoteViewBody extends StatelessWidget {
+  const EditnoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
